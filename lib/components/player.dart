8ea20044958e57158535a2178a420ca8148dcb57
Bunyawat Singchai <bunyawat.s@gmail.com>
2022-10-30 13:39:39 +0700

    Working with the Flame Engine

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -1,55 +1,56 @@
 import 'dart:ui';
 import 'package:flame/components.dart';
+import 'package:flame/geometry.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/palette.dart';
 
-
-class Player extends PositionComponent with HasHitboxes, Collidable  {
+class Player extends PositionComponent with HasHitboxes, Collidable {
   static const int squareSpeed = 250;
-  static final squrePaint = BasicPalette.green.paint();
+  static final squarePaint = BasicPalette.green.paint();
   static const squareWidth = 100.0, squareHeight = 100.0;
-  static const squreWidth = 100.0, squreHight = 100.0;
   late Rect sqarePos;
   int squareDirection = 1;
-  late double screenWidth, screenHeight, centerX, certerY;
-
-  @override
-  void render(Canvas canvas) {
-    canvas.drawRect(sqarePos, squrePaint);
-  }
-
-  @override
-  void update(double dt) {
-    sqarePos = sqarePos.translate(
-      squareSpeed * squareDirection * dt,
-      0,
-    );
-
-    if (squareDirection == 1 && sqarePos.right > screenWidth) {
-      squareDirection = -1;
-    } else if (squareDirection == -1 && sqarePos.left < 0) {
-      squareDirection = 1;
-    }
-  }
+  late double screenWidth, screenHeight, centerX, centerY;
 
   @override
   Future<void> onLoad() async {
     super.onLoad();
+
+    // Get the width and height of our screen canvas
     screenWidth = MediaQueryData.fromWindow(window).size.width;
     screenHeight = MediaQueryData.fromWindow(window).size.height;
-    centerX = (screenWidth / 2) - (squreWidth / 2);
-    certerY = (screenHeight / 2) - (squreHight / 2);
-    sqarePos = Rect.fromLTRB(centerX, certerY, screenWidth, squreHight);
+
+    // Calculate the center of the screen, allowing for the adjustment for the squares size
+    centerX = (screenWidth / 2) - (squareWidth / 2);
+    centerY = (screenHeight / 2) - (squareHeight / 2);
+
+    // Set the initial position of the green square at the center of the screen with a size of 100 width and height
+    position = Vector2(centerX, centerY);
+    size = Vector2(squareWidth, squareHeight);
+
+    addHitbox(HitboxRectangle());
   }
 
-  // @override
-  // void onCollision(Set<Vector2> points, Collidable other) {
-  //   if (other is ScreenCollidable) {
-  //     if (squareDirection == 1) {
-  //       squareDirection = -1;
-  //     } else {
-  //       squareDirection = 1;
-  //     }
-  //   }
-  // }
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, Collidable other) {
+    if (other is ScreenCollidable) {
+      if (squareDirection == 1) {
+        squareDirection = -1;
+      } else {
+        squareDirection = 1;
+      }
+    }
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    position.x += squareSpeed * squareDirection * dt;
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    renderHitboxes(canvas, paint: squarePaint);
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-import 'dart:ui';
 
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/flame.dart';
-import 'package:flame/palette.dart';
+import 'package:goldrush/components/player.dart';
 
 void main() async {
   final goldRush = GoldRush();
@@ -16,41 +16,11 @@ void main() async {
   );
 }
 
-class GoldRush extends Game {
-  static const int squareSpeed = 250;
-  static final squrePaint = BasicPalette.green.paint();
-  static const squareWidth = 100.0, squareHeight = 100.0;
-  static const squreWidth = 100.0, squreHight = 100.0;
-  late Rect sqarePos;
-  int squareDirection = 1;
-  late double screenWidth, screenHeight, centerX, certerY;
-
+class GoldRush extends FlameGame with HasCollidables {
   @override
   Future<void> onLoad() async {
     super.onLoad();
-    screenWidth = MediaQueryData.fromWindow(window).size.width;
-    screenHeight = MediaQueryData.fromWindow(window).size.height;
-    centerX = (screenWidth / 2) - (squreWidth / 2);
-    certerY = (screenHeight / 2) - (squreHight / 2);
-    sqarePos = Rect.fromLTRB(centerX, certerY, screenWidth, squreHight);
-  }
-
-  @override
-  void render(Canvas canvas) {
-    canvas.drawRect(sqarePos, squrePaint);
-  }
-
-  @override
-  void update(double dt) {
-    sqarePos = sqarePos.translate(
-      squareSpeed * squareDirection * dt,
-      0,
-    );
-
-    if (squareDirection == 1 && sqarePos.right > screenWidth) {
-      squareDirection = -1;
-    } else if (squareDirection == -1 && sqarePos.left < 0) {
-      squareDirection = 1;
-    }
+    add(Player());
+    add(ScreenCollidable());
   }
 }
